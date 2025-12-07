0148703284f0619bdc4816755327c5bea347f205
Bryan <bryanwagoki2016@gmail.com>
2025-12-07 20:29:57 +0300

    Style the buttons with flex box

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-let calcObject = {
+// let calcObject = {
 
-inputA : parseInt(prompt("Enter first value")),
-operator : prompt("Enter operator value",""),
-inputB : parseInt(prompt("Enter second value")),
+// inputA : parseInt(prompt("Enter first value")),
+// operator : prompt("Enter operator value",""),
+// inputB : parseInt(prompt("Enter second value")),
 
-add(){
+// add(){
 
-let sum =  this.inputA + this.inputB;
-   return calcObject["inputA"] = sum;
+// let sum =  this.inputA + this.inputB;
+//    return calcObject["inputA"] = sum;
     
-},
+// },
 
-subtract(){
-    let difference = this.inputA - this.inputB;
-   return calcObject["inputA"] = difference;
-},
+// subtract(){
+//     let difference = this.inputA - this.inputB;
+//    return calcObject["inputA"] = difference;
+// },
 
-multiply(){
-    let product = this.inputA * this.inputB;
-   return calcObject["inputA"] = product; 
-},
+// multiply(){
+//     let product = this.inputA * this.inputB;
+//    return calcObject["inputA"] = product; 
+// },
 
-division(){
-    let quotient = this.inputA / this.inputB;
-   return calcObject["inputA"] = quotient; 
-}
+// division(){
+//     let quotient = this.inputA / this.inputB;
+//    return calcObject["inputA"] = quotient; 
+// }
 
-}
+// }
 
 
 
