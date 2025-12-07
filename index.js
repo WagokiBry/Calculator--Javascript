400db3d645b4e40ced13535b1f88187caaea19c7
Bryan <bryanwagoki2016@gmail.com>
2025-12-07 18:45:40 +0300

    Create the basic functions to add, subtract, multiply and divide

---
+++ index.js
@@ -0,0 +1,25 @@
+// add
+// subtract
+// multiply
+// divide
+
+function add(a,b){
+    return a + b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+console.log(add(5,7)) 
+console.log(subtract(9,4))
+console.log(multiply(8,9))
+console.log(divide(20,4))
